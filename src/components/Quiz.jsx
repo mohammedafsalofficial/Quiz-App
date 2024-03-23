bb9b094ad126dfd8d05409bbc635a91d04f4fc4e
Mohammed Afsal <mohammedafsaltut@gmail.com>
2024-03-23 11:16:21 +0530

    Add Summary component to display the end result to the user

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,7 +1,7 @@
 import { useCallback, useState } from "react";
 import QUESTIONS from "../question.js";
-import quizCompleteImg from "../assets/quiz-complete.png";
 import Question from "./Question.jsx";
+import Summary from "./Summary.jsx";
 
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
@@ -19,12 +19,7 @@ export default function Quiz() {
   const handleSkipAnswer = useCallback(() => handleSelectAnswer(null), [handleSelectAnswer]);
 
   if (quizIsComplete) {
-    return (
-      <div id="summary">
-        <img src={quizCompleteImg} alt="Trophy icon" />
-        <h2>Quiz completed!</h2>
-      </div>
-    );
+    return <Summary userAnswers={userAnswers} />;
   }
 
   return (
